e3a0264fc8cecab73025109db336fd1ca2da8a59
ddeveza <ddeveza@ymail.com>
2022-01-25 09:45:21 +0800

    add localhost

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+    console.log(app);
     app.use (
         createProxyMiddleware("/beta/devices",{
             target:"https://graph.microsoft.com",
@@ -8,7 +9,8 @@ module.exports = function (app) {
             ws:true,
             changeOrigin:true,
             router:{
-                'dev.localhost.3000': 'https://orange-forest-0b82bff10.1.azurestaticapps.net/'
+                'https://orange-forest-0b82bff10.1.azurestaticapps.net/': 'https://graph.microsoft.com' ,
+                
             }
         })
     )
